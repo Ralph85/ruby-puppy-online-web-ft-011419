7a415669386a6c147561807192e8538686994317
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-04-22 23:23:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,22 @@
 class Dog
 
-  @@all = []
-
     attr_accessor :name
+
+    @@all = []
 
     def initialize(name)
     @name = name
     @@all << self
-    pluto = Dog.new("Pluto")
-    fido = Dog.new("Fido")
-    maddy = DOg.new("Maddy")
-  end
-
-  def self.destroy_all
-    @@all.clear
-
   end
 
   def self.all
-    @@all
-    puts .all
+    @@all.each do |dog|
+    puts dog.name
   end
+
+  def self.clear_all
+    @@all.clear
+  end
+
 end
+
